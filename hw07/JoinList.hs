caca1bd9c8cd2e9cc99974ccdb0f2a8617cc18e1
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-26 13:06:19 +0200

    Add indexJ implementation (ex2)

--- hw07/JoinList.hs
+++ hw07/JoinList.hs
@@ -1,5 +1,9 @@
 module JoinList where
 
+import Sized
+
+-- ex 1
+
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
@@ -14,6 +18,27 @@ jl1 +++ jl2 =
     Append (mappend m1 m2) jl1 jl2
 
 getTag :: Monoid m => JoinList m a -> m
-getTag Empty = mempty
-getTag (Single m a) = m
+getTag Empty          = mempty
+getTag (Single m a)   = m
 getTag (Append m _ _) = m
+
+-- ex 2
+
+indexJ :: (Sized b, Monoid b) =>
+          Int -> JoinList b a -> Maybe a
+indexJ _ Empty = Nothing
+indexJ i (Single m a)
+  | i == 0    = Just a
+  | otherwise = Nothing
+indexJ i (Append m jl1 jl2)
+  | i < 0 || (size m) <= (Size i) = Nothing
+  | otherwise =
+    let 
+      m1  = getTag jl1
+      sm1 = size m1
+      si  = Size i
+    in
+      if si < sm1 then
+        indexJ i jl1
+      else
+        indexJ (getSize (si - sm1)) jl2
