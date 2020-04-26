4b6d2f77dd49c6307f426d9b350500767f23a9f4
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-26 13:06:19 +0200

    Add second function for assignment4 exercise 1

--- hw04/Higher-Order.hs
+++ hw04/Higher-Order.hs
@@ -1,3 +1,6 @@
 fun1' :: [Integer] -> Integer
 fun1' = product . map (subtract 2) . filter even
 
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (>1) .
+        iterate (\x -> if even x then div x 2 else 3 * x + 1)
