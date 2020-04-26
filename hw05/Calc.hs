72b1c146cfce2326dd0250c353008a5e5a1a948c
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-26 13:03:07 +0200

    Add Expr typeclass with instance for ExprT

--- hw05/Calc.hs
+++ hw05/Calc.hs
@@ -8,3 +8,13 @@ eval (Mul e1 e2) = eval e1 * eval e2
 
 evalStr :: String -> Maybe Integer
 evalStr s = fmap eval $ (parseExp Lit Add Mul) s
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
