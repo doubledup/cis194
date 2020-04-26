31da3276913cfe728d2c92c92a8f0555af72aebf
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-26 13:03:07 +0200

    Add Expr instances for different semantics

--- hw05/Calc.hs
+++ hw05/Calc.hs
@@ -18,3 +18,27 @@ instance Expr ExprT where
   lit = Lit
   add = Add
   mul = Mul
+
+-- e4
+newtype MinMax = MinMax Integer deriving (Show, Eq)
+newtype Mod7 = Mod7 Integer deriving (Show, Eq)
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (>0)
+  add = (||)
+  mul = (&&)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+instance Expr Mod7 where
+  lit = Mod7 . flip mod 7
+  add (Mod7 x) (Mod7 y) = Mod7 . flip mod 7 $ x + y
+  mul (Mod7 x) (Mod7 y) = Mod7 . flip mod 7 $ x * y
