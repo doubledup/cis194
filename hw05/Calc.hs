50d194d6146123d2ef96a9898a2fb8689c8a7013
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-26 13:03:07 +0200

    Implement evalStr

--- hw05/Calc.hs
+++ hw05/Calc.hs
@@ -1,6 +1,10 @@
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit i) = i
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
+
+evalStr :: String -> Maybe Integer
+evalStr s = fmap eval $ (parseExp Lit Add Mul) s
