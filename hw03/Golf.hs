96fc7246381160d6708c2b36c01032116066399b
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-26 13:06:19 +0200

    Shorten function names 'for to win the golf'

--- hw03/Golf.hs
+++ hw03/Golf.hs
@@ -24,25 +24,25 @@ localMaxima (a:b:c:x)
 
 --3-----------------------------------------------------------------------------
 -- Given a list l and a number n, count the occurrences of n in l
-countn :: [Integer] -> Integer -> Integer
-countn [] _ = 0
-countn l n = toInteger . length $ filter (\x -> x == n) l
+c :: [Integer] -> Integer -> Integer
+c [] _ = 0
+c l n = toInteger . length $ filter (\x -> x == n) l
 
 -- Given a list l and a pair of inclusive bounds (low, hi), return the number
 -- of times each number in the bounds appears in l.
-freq :: [Integer] -> (Integer, Integer) -> [Integer]
-freq l (low, hi) = map (countn l) [low..hi]
+f :: [Integer] -> (Integer, Integer) -> [Integer]
+f l (low, hi) = map (c l) [low..hi]
 
-graph :: [Integer] -> String
-graph [] = ""
-graph l
+g :: [Integer] -> String
+g [] = ""
+g l
   | length l == length (filter (\x -> x == 0) l) = ""
-  | otherwise = graph (map (\x -> if x > 0 then x - 1 else x) l)
+  | otherwise = g (map (\x -> if x > 0 then x - 1 else x) l)
                 ++ "\n"
                 ++ (map (\x -> if x > 0 then '*' else ' ') l)
 
 histogram :: [Integer] -> String
-histogram l = graph (freq l (0,9)) ++ "\n==========\n0123456789\n"
+histogram l = g (f l (0,9)) ++ "\n==========\n0123456789\n"
 
 main :: IO()
 main = do
@@ -63,6 +63,6 @@ main = do
   --print $ localMaxima [1..5]
   --print $ localMaxima [9,3,5,4,7,7,7,45,3]
 
-  print $ countn [3,6,5,7,4,1,3,3,6,5,2,8,9,0,7,3] 3
+  print $ c [3,6,5,7,4,1,3,3,6,5,2,8,9,0,7,3] 3
 
   print $ histogram [1,1,1,5]
