dcaa7007a975ed12cec157caa205755d0b37c1a3
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-09-24 19:59:17 +0200

    Add doubleEveryOther

--- 1-to-4-checksum.hs
+++ 1-to-4-checksum.hs
@@ -6,4 +6,14 @@ toDigits i = charsToIntegers . show $ i
 charsToIntegers :: [Char] -> [Integer]
 charsToIntegers = (map $ toInteger . digitToInt)
 
-main = print . toDigits $ 421
+-- main = print . toDigits $ 421
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = reverse . doubleEveryOtherFromLeft . reverse
+
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft [] = []
+doubleEveryOtherFromLeft (x:[]) = [x]
+doubleEveryOtherFromLeft (x:y:xs) = x : (2*y) : doubleEveryOtherFromLeft xs
+
+main = print $ doubleEveryOther [1, 1, 2, 3, 5, 8, 13]
